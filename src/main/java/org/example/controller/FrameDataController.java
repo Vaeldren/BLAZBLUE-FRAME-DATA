c0ac8e6f0fb4830d1d029abd6997804718ce8a13
vaeldren <abraham-aidid@hotmail.co.uk>
2025-01-12 22:39:52 +0000

    scraper done but needs testing. controller and service have added functionality.

--- src/main/java/org/example/controller/FrameDataController.java
+++ src/main/java/org/example/controller/FrameDataController.java
@@ -3,11 +3,9 @@ package org.example.controller;
 import org.example.model.FrameData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,4 +29,23 @@ public class FrameDataController {
         FrameData frameData = frameDataService.getFrameDataByCharacterAndInput(characterName,input);
         return ResponseEntity.ok(frameData);
     }
+
+    @GetMapping("/{characterName}/{input}/image")
+    public ResponseEntity<ArrayList<String>> getImage(@PathVariable String characterName, @PathVariable String input){
+        ArrayList<String> image = frameDataService.getImageByCharacterAndInput(characterName, input);
+        return ResponseEntity.ok(image);
+    }
+
+    @PostMapping("/{characterName}/{input}")
+    public ResponseEntity<FrameData> addFrameData (@RequestBody FrameData frameData){
+        frameDataService.save(frameData);
+        return ResponseEntity.ok(frameData);
+    }
+    @PostMapping("/batch")
+    public ResponseEntity<String> saveFrameDataBatch(@RequestBody List<FrameData> frameDataList){
+        frameDataService.saveAll(frameDataList);
+        return ResponseEntity.ok("Batch data saved successfully");
+    }
+
+
 }
